b6544f7bc01dc2fa7fd13e0f08952330426afae4
Naichen Wang <wangroger0801@outlook.com>
2019-07-03 17:48:44 +0200

    update versions and cid settings

--- 07-03-1stTest/script/braketest/braketest-up.sh
+++ 07-03-1stTest/script/braketest/braketest-up.sh
@@ -2,6 +2,8 @@
 
 docker-compose -f braketest.yml down
 
+docker-compose -f camera-imu-data-collection.yml down
+
 docker-compose -f braketest.yml up -d
 
 docker-compose -f camera-imu-data-collection.yml up -d
