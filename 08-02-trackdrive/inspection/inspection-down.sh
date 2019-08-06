e09b6ebf0991a082ca43307e0c56204b1123e39d
Naichen Wang <wangroger0801@outllok.com>
2019-08-06 17:12:37 +0200

    08-02 longitudinal control and trackdrive test usecases

---
+++ 08-02-trackdrive/inspection/inspection-down.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sleep 5
+
+docker-compose -f inspection.yml down
+
+
---
+++ 08-02-trackdrive/trackdrive/trackdrive-up.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker-compose -f trackdrive.yml down
+
+docker-compose -f trackdrive.yml up -d 
+
+
